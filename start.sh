a84ba61efd2836a242ff127d3c9e7e44ee0bdc0e
Alessio Galatolo <galatoloalessio@gmail.com>
2021-06-24 16:00:26 +0200

    Add support for custom gunicorn conf

--- start.sh
+++ start.sh
@@ -17,6 +17,7 @@ else
     echo "$wwwdomains"
 fi
 
+#set tls if available
 if [ -n "$use_tls" ] && [ "$use_tls" = "true" ] ; then
     echo "Using tls"
     template_str=$(cat "nginx_ssl_template.conf")
@@ -25,5 +26,12 @@ else
     template_str=$(cat "nginx_template.conf")
 fi
 
+#set the nginx confog
 eval "echo \"${template_str}\"" > /etc/nginx/conf.d/nginx.conf
+
+#set gunicorn conf if available
+if [ -n "$gunicorn_config_file" ] ; then
+    cat /gunicorn.conf.py $gunicorn_config_file > /gunicorn.conf.py
+fi
+
 exec supervisord -c /supervisord.conf
